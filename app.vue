95126d0f143d0523c1280244966f53ca54cfc6fd
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-11-15 09:34:02 +0000

    chore: add some schema-org config

--- app.vue
+++ app.vue
@@ -19,19 +19,38 @@ useHead({
   ],
 })
 
+useSchemaOrg([
+  defineWebPage({
+    name: 'Abdelilah - Portfolio',
+    description: 'Welcome to my personal portfolio',
+  }),
+  definePerson({
+    name: 'Abdelilah ouaadouch',
+    jobTitle: 'Backend developer',
+    email: 'mailto:abdelilah4dev@gmail.com',
+    telephone: '+212603539796',
+    url: 'https://ar7al.com',
+    sameAs: [
+      'https://www.linkedin.com/in/abdelilah-ouaadouch-464060295/',
+      'https://github.com/AbdelilahOu',
+      'https://x.com/Abdelilah4dev',
+    ],
+  }),
+])
+
 useSeoMeta({
   title: 'Abdelilah Ouaadouch',
   description:
-    'Backend Developer specializing in Go (Golang) and Typescript. Building fast, reliable systems.',
+    'Personal website of Abdelilah ouaadouch - Developer specializing in Typescript + Nuxt and Golang',
   twitterTitle: 'Abdelilah Ouaadouch - Backend Developer',
   twitterDescription:
-    'Backend Developer specializing in Go (Golang) and Typescript. Building fast, reliable systems.',
+    'Personal website of Abdelilah ouaadouch - Developer specializing in Typescript + Nuxt and Golang',
   twitterCard: 'summary_large_image',
   twitterImage: '/og.png',
   twitterCreator: '@Abdelilah4dev',
   ogTitle: 'Abdelilah Ouaadouch - Backend Developer',
   ogDescription:
-    'Backend Developer specializing in Go (Golang) and Typescript. Building fast, reliable systems.',
+    'Personal website of Abdelilah ouaadouch - Developer specializing in Typescript + Nuxt and Golang',
   ogType: 'website',
   ogSiteName: 'Abdelilah Ouaadouch',
   ogLocale: 'en',
