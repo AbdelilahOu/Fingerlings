2c38f5f4f05ce818ed277d98e4be40de37200358
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-11-28 21:33:09 +0000

    feat: add simple effect

---
+++ components/ProjectCard.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+defineProps<{
+  project: {
+    title: string;
+    description: string;
+    tech: string[];
+    github?: string;
+    web?: string;
+  };
+}>();
+</script>
+
+<template>
+  <div class="bg-zinc-800 relative p-[20px]">
+    <ClientOnly>
+      <BackgroundGrid class="absolute inset-0 z-0" />
+    </ClientOnly>
+
+    <article class="relative z-10">
+      <h3 class="text-2xl block w-full font-medium text-white">
+        {{ project.title }}
+      </h3>
+      <p class="text-slate-300 block mt-2 text-xl">
+        {{ project.description }}
+      </p>
+      <div class="mt-3 flex flex-wrap gap-2">
+        <span
+          v-for="tech in project.tech"
+          :key="tech"
+          class="px-2 py-1 bg-zinc-600 text-slate-300 text-sm"
+        >
+          {{ tech }}
+        </span>
+      </div>
+      <div class="space-x-5">
+        <NuxtLink
+          v-if="project.github"
+          :to="project.github"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="inline-block mt-3 text-blue-400 hover:text-blue-300"
+        >
+          View on GitHub →
+        </NuxtLink>
+        <NuxtLink
+          v-if="project.web"
+          :to="project.web"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="inline-block mt-3 text-blue-400 hover:text-blue-300"
+        >
+          View Live →
+        </NuxtLink>
+      </div>
+    </article>
+  </div>
+</template>
