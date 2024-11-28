31bebb908f2a6aa350248ed2542b0abf4d2c6cad
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-11-28 22:09:06 +0000

    chore: an other variantion

--- components/BackgroundGrid.vue
+++ components/BackgroundGrid.vue
@@ -15,6 +15,56 @@ const height = computed(() => svgElement.value?.clientHeight || 768);
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
+    <template v-for="x in Math.ceil(width / 16)">
+      <rect
+        v-for="y in Math.floor(Math.random() * 5 + 1)"
+        :key="`${x}-${y}`"
+        :x="(x - 1) * 16"
+        :y="height - 16 * y"
+        width="16"
+        height="16"
+        :style="{
+          animationDelay: `${Math.random() * 2000}ms`,
+          fill: bgColor || '#0c0c0c',
+          stroke: bgColor || '#0c0c0c',
+        }"
+        class="opacity-10 absolute animate-pulse duration-500"
+      ></rect>
+      <rect
+        v-for="y in Math.floor(Math.random() * 5 + 1)"
+        :key="`${x}-${y}`"
+        :x="(x - 1) * 16"
+        :y="16 * (y - 1)"
+        width="16"
+        height="16"
+        :style="{
+          animationDelay: `${Math.random() * 2000}ms`,
+          fill: bgColor || '#0c0c0c',
+          stroke: bgColor || '#0c0c0c',
+        }"
+        class="opacity-10 absolute animate-pulse duration-500"
+      ></rect>
+    </template>
+  </svg>
+</template>
+
+<!-- <script setup lang="ts">
+defineProps<{
+  bgColor?: string;
+}>();
+
+const svgElement = ref<HTMLElement | null>(null);
+const width = computed(() => svgElement.value?.clientWidth || 768);
+const height = computed(() => svgElement.value?.clientHeight || 768);
+</script>
+
+<template>
+  <svg
+    ref="svgElement"
+    class="absolute left-0 top-0 z-0 w-full h-full"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <template v-for="y in Math.ceil(height / 16)">
       <rect
         v-for="x in Math.ceil(width / 16)"
@@ -32,4 +82,4 @@ const height = computed(() => svgElement.value?.clientHeight || 768);
       ></rect>
     </template>
   </svg>
-</template>
+</template> -->
