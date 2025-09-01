4160e804eefb70eb06697e4eef8ff9290e35704b
Ar7al <86406783+AbdelilahOu@users.noreply.github.com>
2025-09-01 20:02:45 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,4 +26,14 @@ module.exports = {
       },
     },
   },
+   plugins: [
+    require('tailwindcss/plugin')(({ addUtilities }) => {
+      const newUtilities = {
+        '.clip-cut-corners': {
+          'clip-path': 'polygon(10% 0, 90% 0, 100% 10%, 100% 90%, 90% 100%, 10% 100%, 0 90%, 0 10%)',
+        },
+      }
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    }),
+  ],
 };
