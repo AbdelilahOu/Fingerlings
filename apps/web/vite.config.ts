74ac457f3dc0eb4006c11bab454bb8f43485382b
abdelilah ouaadouch <abdelilah4dev@gmail.com>
2026-01-25 19:43:59 +0000

    chore: clean up

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import { sveltekitOG } from '@ethercorps/sveltekit-og/plugin';
-
+import { sveltekitOG } from "@ethercorps/sveltekit-og/plugin";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit(), sveltekitOG()],
