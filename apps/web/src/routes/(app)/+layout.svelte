f6aa9cad8677a3aad3bfd3378f210f5ea60e9190
abdelilah ouaadouch <abdelilah4dev@gmail.com>
2026-01-26 14:52:45 +0000

    feat: try again

--- apps/web/src/routes/(app)/+layout.svelte
+++ apps/web/src/routes/(app)/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import Nav from '$lib/components/Nav.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
 
 	let { children } = $props();
 </script>
 
 <div class="min-h-screen w-screen bg-[#0d0d0d] px-4 py-8">
 	<div class="m-auto w-full max-w-3xl space-y-8">
-		<Nav />
+		<Navbar />
 		{@render children()}
 	</div>
 </div>
