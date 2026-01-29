56db9c008e9c7849df89c006695193c0e903b946
abdelilah ouaadouch <abdelilah4dev@gmail.com>
2026-01-29 13:47:31 +0000

    feat: fix

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -38,9 +38,7 @@ const config = {
   preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
   kit: {
-    adapter: alchemy({
-      config: "wrangler.toml"
-    }),
+    adapter: alchemy(),
     alias: {
       "@posts": "src/posts",
     },
