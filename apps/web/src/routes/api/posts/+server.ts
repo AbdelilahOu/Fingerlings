5893a987947d57084d41a44d733295e8acd5244c
abdelilah ouaadouch <abdelilah4dev@gmail.com>
2026-02-02 11:27:39 +0000

    chore: clean up

--- apps/web/src/routes/api/posts/+server.ts
+++
@@ -1,9 +0,0 @@
-import { getPosts } from "$lib/data/posts";
-import { json } from "@sveltejs/kit";
-
-export const prerender = true;
-
-export function GET() {
-  const posts = getPosts();
-  return json(posts);
-}
